9d65bb49e73dde7c56a0c73d85ad6dd515ace74d
AhmedFathyQa <ahmed.fathi.abbas@gmail.com>
2023-05-06 13:39:20 +0200

    adding new ling

--- src/test/java/TestCases/PurchaseFullCycle.java
+++ src/test/java/TestCases/PurchaseFullCycle.java
@@ -63,6 +63,7 @@ public class PurchaseFullCycle extends BaseTest {
         cp.clickOnConfirmBtn ();
 
         Assert.assertEquals ( sp.getSuccessMsg () , "Your order has been successfully processed!" );
+        System.out.println ("hello from branching" );
     }
     }
 
